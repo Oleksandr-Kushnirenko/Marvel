0049677053dae5362db3368932c7732b41088ca3
Kushnirenko Oleksandr <kushnirenko.oleksandr@gmail.com>
2022-08-31 16:21:59 +0300

    Lesson 173 React Router v6

---
+++ src/components/pages/ComicsPage.js
@@ -0,0 +1,16 @@
+
+
+import ComicsList from "../comicsList/ComicsList";
+import AppBanner from "../appBanner/appBanner";
+
+const ComicsPage = () => {
+
+    return (
+        <>
+            <AppBanner/>
+            <ComicsList/>
+        </>
+    )
+}
+
+export default ComicsPage;
