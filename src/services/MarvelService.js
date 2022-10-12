f61b990e982d76711a5e99d9ab7ecc2540a72754
Oleksandr Kushnirenko <kushnirenko.oleksandr@gmail.com>
2022-10-12 14:52:02 +0300

    Lesson 185 Add search form

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -13,6 +13,11 @@ const useMarvelService = () => {
         return res.data.results.map(_transformChatacter);
     }
 
+    const getCharacterByName = async (name) => {
+        const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+        return res.data.results.map(_transformCharacter);
+    }
+
     const getCharacter = async (id) => {
         const res = await request(`${_apiBase}characters/${id}?&${_apiKey}`);
         return _transformChatacter(res.data.results[0]);
@@ -40,6 +45,18 @@ const useMarvelService = () => {
         } 
     }
 
+    const _transformCharacter = (char) => {
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : "here is no description for this character",
+            thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+            comics: char.comics.items
+        }
+    }
+
     const _transformComics = (comics) => {
         return {
             id: comics.id,
@@ -52,7 +69,7 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComic} 
+    return {loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComic, getCharacterByName} 
 }
 
 export default useMarvelService;
