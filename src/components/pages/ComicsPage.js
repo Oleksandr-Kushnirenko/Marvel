95ab67d38a12f4f6893652cf13660b10ac452130
Oleksandr Kushnirenko <kushnirenko.oleksandr@gmail.com>
2022-10-12 19:33:12 +0300

    Lesson 186. SEO optimization, React-helmet

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,4 +1,4 @@
-
+import {Helmet} from "react-helmet";
 import ComicsList from "../comicsList/ComicsList";
 import AppBanner from "../appBanner/appBanner";
 
@@ -6,6 +6,13 @@ const ComicsPage = () => {
 
     return (
         <>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Page with list of our comics"
+                />
+                <title>Comics page</title>
+            </Helmet>
             <AppBanner/>
             <ComicsList/>
         </>
